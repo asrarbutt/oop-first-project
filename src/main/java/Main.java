5900c7a25303a672e0c8fb6f64f76c18fe96c5cc
asrarbutt <asrarbutt1986@gmail.com>
2022-07-04 16:19:53 +0200

    add test for equals

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student student=new Student("Jan", "hoffmann", 26);
-        System.out.println(student);
+        Student student=new Student("Ja", "hoffmann", 26);
+        Student student2=new Student("Jan", "hoffmann", 26);
+
+
+        System.out.println(student.equals(student2));
 
 
 
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,33 @@
+import model.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+
+    @Test
+    void toStringTest(){
+        //given
+        Student student=new Student("Martin", "Mueller", 35);
+
+        //when
+        String actual=student.toString();
+        String expected="Firstname: Martin Lastname: Mueller Age: 35";
+
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void testEqualsFromStudent(){
+        //given
+        Student student1=new Student("Jan", "hoffmann", 25);
+        Student student2=new Student("Jan", "hoffmann", 25);
+
+        //when
+        boolean actual=student1.equals(student2);
+
+        //then
+        Assertions.assertEquals(true, actual);
+    }
+}
