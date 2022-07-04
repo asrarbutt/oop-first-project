91d5fcf69ce6e8d466eb60774672b55c3aa51d13
asrarbutt <asrarbutt1986@gmail.com>
2022-07-04 11:53:09 +0200

    add variable and getter/setter in student

---
+++ src/main/java/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    String firstName, secondName;
+    int age;
+
+
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+}
