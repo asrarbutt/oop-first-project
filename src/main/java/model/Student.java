ba0970c1fe399bfc62ccbc3c5e6fdec04bf0fcb4
asrarbutt <asrarbutt1986@gmail.com>
2022-07-04 16:15:22 +0200

    add equals and hashcode

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Student {
 
    private String firstName, lastName;
@@ -51,6 +53,19 @@ public class Student {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
     }
 
     @Override
